a9693e6a2e1e77b53797fb7ef8ca0e8081129627
drpoudel3785 <dharmarajpoudel@hotmail.com>
2025-12-18 11:55:11 +0545

    user registration

--- vscode/app.py
+++ vscode/app.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path="/")
+
+import mysql.connector
+# 1. Connect
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="P@ssw0rd",
+    database="ai2025a",
+    port=3307
+)
 
 @app.route( '/')
 def index():
@@ -12,6 +22,26 @@ def about():
 @app.route( '/contact')
 def contact():
    return render_template("contact.html")
+
+@app.route( '/login')
+def login():
+   return render_template("login.html")
+
+@app.route( '/register', methods=['GET', 'POST'])
+def register():
+   msg = ''
+   if request.method == 'POST':
+
+      username = request.form['username']
+      password = request.form['password']
+      email = request.form['email']
+      cursor = db.cursor()
+      cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
+      db.commit()
+      return redirect(url_for('login'))
+      #return "from submitted"
+   else:
+      return render_template("register.html")
 
 @app.route('/booking/<int:id>')
 def booking(id):
